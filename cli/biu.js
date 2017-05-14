7306d014b1547e2b932269129b139c67319e398c
vilicvane <i@vilic.info>
2017-05-14 20:24:20 +0800

    Update usage text

--- cli/biu.js
+++ cli/biu.js
@@ -18,7 +18,7 @@ program
     .version(require('../package.json').version)
     .usage('[biu.json]')
     .option('-o, --open-browser', 'Open browser')
-    .option('-p, --port <port>', 'Open browser', /^\d+$/, 8088)
+    .option('-p, --port <port>', 'Port to listen', /^\d+$/, 8088)
     .parse(process.argv);
 
 var biuFilePath = program.args[0] || 'biu.json';
@@ -52,16 +52,16 @@ io.on('connection', function (socket) {
         executors.forEach(function (executor) {
             executor.stop(true);
         });
-        
+
     } else {
         if (socket.handshake.query.everConnected === 'true') {
             socket.emit('reload');
             return;
         }
-        
+
         commandsStarted = true;
     }
-    
+
     initializeSocket(socket);
     startCommands();
 });
@@ -81,17 +81,17 @@ if (program.openBrowser) {
 
 function initializeSocket(socket) {
     socket.join('biu');
-    
+
     socket.on('restart-command', function (data) {
         var executor = executors[data.id];
         executor.restart();
     });
-    
+
     socket.on('start-command', function (data) {
         var executor = executors[data.id];
         executor.start();
     });
-    
+
     socket.on('stop-command', function (data) {
         var executor = executors[data.id];
         executor.stop();
@@ -102,54 +102,54 @@ function startCommands() {
     room.emit('initialize', {
         commands: commands
     });
-    
+
     executors = commands.map(function (options, index) {
         var command = options.command;
-        
+
         console.log('Executing command `' + options.display + '`.');
-        
+
         var stdout, stderr;
-        
+
         if (options.stdout) {
             stdout = process.stdout;
             stderr = process.stderr;
         }
-        
+
         var executor = new Executor(command, options.args, options.npm, options.cwd, stdout, stderr);
-        
+
         executor.on('start', function () {
             room.emit('command-start', {
                 id: index
             });
         });
-        
+
         executor.on('exit', function (error) {
             room.emit('command-exit', {
                 id: index,
                 error: error && error.stack
             });
-            
+
             if (error) {
                 console.error(error.stack);
             }
         });
-        
+
         executor.on('stdout', function (data) {
             room.emit('stdout', {
                 id: index,
                 html: ansiConverter.toHtml(data.toString())
             });
         });
-        
+
         executor.on('stderr', function (data) {
             room.emit('stderr', {
                 id: index,
                 html: ansiConverter.toHtml(data.toString())
             });
         });
-        
+
         executor.start();
-        
+
         return executor;
     });
 }

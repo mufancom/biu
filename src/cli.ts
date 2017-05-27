f67b0f06fe7a9c7529683f4524eba9ddc68b2dec
vilicvane <i@vilic.info>
2017-05-27 17:25:04 +0800

    Handle uncaughtException gently

--- src/cli.ts
+++ src/cli.ts
@@ -4,8 +4,14 @@ import 'source-map-support/register';
 import 'villa/platform/node';
 
 import * as Path from 'path';
+import * as Util from 'util';
 
 import { CLI, Shim } from 'clime';
+
+process.on('uncaughtException', (error: any) => {
+  process.stderr.write(`${Util.inspect(error)}\n`);
+  process.exit(1);
+});
 
 let cli = new CLI('biu', Path.join(__dirname, 'commands'));
 

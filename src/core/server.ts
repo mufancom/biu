08982bf8b8e7edabe1eeb897ad6fa888229689b4
emiwang <2585026122@qq.com>
2019-03-20 18:32:13 +0800

    before exit, kill all tasks

--- src/core/server.ts
+++ src/core/server.ts
@@ -176,6 +176,18 @@ export class Server extends EventEmitter {
   private setup(): void {
     this.app.use(express.static(Path.join(__dirname, '../../static')));
     this.io.on('connection', socket => this.initializeConnection(socket));
+
+    let onSIGINTTERM = (): void => {
+      this.stopAll().then(
+        () => process.exit(),
+        error => {
+          console.error(error);
+          process.exit(1);
+        },
+      );
+    };
+
+    process.on('SIGINT', onSIGINTTERM);
   }
 
   private outputProblems(owner: string): void {
